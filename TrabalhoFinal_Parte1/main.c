d786a0ee531a3e3c69ceb86a3028557fd05fa80a
voliverx <jose.vitor@estudante.iftm.edu.br>
2024-08-08 13:07:24 -0300

    Parte 1 completo

--- TrabalhoFinal_Parte1/main.c
+++ TrabalhoFinal_Parte1/main.c
@@ -1,3 +1,21 @@
+/*
+ * Trabalho Final Parte 1 de Programação Estruturada
+ *
+ * Autores: José Vitor Oliveira Reis e Vinícius Nunes de Lima Gabriel
+ * Data: 7 de agosto de 2024
+ *
+ * Descrição:
+ * Este programa realiza operações de manipulação em imagens no formato PPM.
+ * As operações incluem:
+ * - Inversão horizontal
+ * - Inversão vertical
+ * - Divisão vertical
+ * - Divisão horizontal
+ *
+ * Nota:
+ * Certifique-se de que o arquivo "imagem.ppm" está no diretório correto antes de executar o programa.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,8 +24,8 @@ int escalaCinza()
     int largura, altura, valor_maximo;
     char buffer[255];
 
-    FILE *arquivo = fopen("tucano.ppm", "r");
-    FILE *saida = fopen("tucano_cinza.ppm", "w");
+    FILE *arquivo = fopen("imagem.ppm", "r");
+    FILE *saida = fopen("imagem_cinza.ppm", "w");
 
     if (arquivo == NULL)
     {
@@ -80,8 +98,8 @@ int escalaCinza()
 int inverteHorizontal()
 {
     FILE *entrada, *saida;
-    entrada = fopen("tucano.ppm", "r");
-    saida = fopen("tucano_virado_horizontal.ppm", "w");
+    entrada = fopen("imagem.ppm", "r");
+    saida = fopen("imagem_virado_horizontal.ppm", "w");
 
     int i, j, k;
     int largura, altura, v_max;
@@ -143,8 +161,8 @@ int inverteHorizontal()
 int inverteVertical()
 {
     FILE *entrada, *saida;
-    entrada = fopen("tucano.ppm", "r");
-    saida = fopen("tucano_virado_vertical.ppm", "w");
+    entrada = fopen("imagem.ppm", "r");
+    saida = fopen("imagem_virado_vertical.ppm", "w");
 
     int i, j, k;
     int largura, altura, v_max;
@@ -203,6 +221,163 @@ int inverteVertical()
     return 0;
 }
 
+int divisaoVertical()
+{
+    FILE *entrada, *saidaEsquerda, *saidaDireita;
+    entrada = fopen("imagem.ppm", "r");
+    saidaEsquerda = fopen("imagem_metade_esquerda.ppm", "w");
+    saidaDireita = fopen("imagem_metade_direita.ppm", "w");
+
+    if (entrada == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    int largura, altura, v_max;
+    char buffer[255];
+
+    // Leitura do cabeçalho
+    fgets(buffer, 255, entrada);
+    fprintf(saidaEsquerda, "%s", buffer);
+    fprintf(saidaDireita, "%s", buffer);
+
+    fgets(buffer, 255, entrada);
+    fprintf(saidaEsquerda, "%s", buffer);
+    fprintf(saidaDireita, "%s", buffer);
+
+    fscanf(entrada, "%d %d", &largura, &altura);
+    fprintf(saidaEsquerda, "%d %d\n", largura / 2, altura);
+    fprintf(saidaDireita, "%d %d\n", largura / 2, altura);
+
+    fscanf(entrada, "%d", &v_max);
+    fprintf(saidaEsquerda, "%d\n", v_max);
+    fprintf(saidaDireita, "%d\n", v_max);
+
+    int matriz[largura][3];
+
+    // Processamento dos pixels
+    for (int i = 0; i < altura; i++)
+    {
+        for (int j = 0; j < largura; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                fscanf(entrada, "%d", &matriz[j][k]);
+            }
+        }
+
+        // Gravar metade esquerda
+        for (int j = 0; j < largura / 2; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                fprintf(saidaEsquerda, "%d ", matriz[j][k]);
+            }
+        }
+        fprintf(saidaEsquerda, "\n");
+
+        // Gravar metade direita
+        for (int j = largura / 2; j < largura; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                fprintf(saidaDireita, "%d ", matriz[j][k]);
+            }
+        }
+        fprintf(saidaDireita, "\n");
+    }
+
+    printf("Imagem dividida com sucesso.\n");
+
+    fclose(entrada);
+    fclose(saidaEsquerda);
+    fclose(saidaDireita);
+
+    return 0;
+}
+
+int divisaoHorizontal()
+{
+    FILE *entrada, *saidaSuperior, *saidaInferior;
+    entrada = fopen("imagem.ppm", "r");
+    saidaSuperior = fopen("imagem_metade_superior.ppm", "w");
+    saidaInferior = fopen("imagem_metade_inferior.ppm", "w");
+
+    if (entrada == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    int largura, altura, v_max;
+    char buffer[255];
+
+    // Leitura do cabeçalho
+    fgets(buffer, 255, entrada); // Tipo P3
+    fprintf(saidaSuperior, "%s", buffer);
+    fprintf(saidaInferior, "%s", buffer);
+
+    fgets(buffer, 255, entrada); // Comentário
+    fprintf(saidaSuperior, "%s", buffer);
+    fprintf(saidaInferior, "%s", buffer);
+
+    fscanf(entrada, "%d %d", &largura, &altura); // Largura e altura
+    fprintf(saidaSuperior, "%d %d\n", largura, altura / 2);
+    fprintf(saidaInferior, "%d %d\n", largura, altura / 2);
+
+    fscanf(entrada, "%d", &v_max); // Valor máximo
+    fprintf(saidaSuperior, "%d\n", v_max);
+    fprintf(saidaInferior, "%d\n", v_max);
+
+    int linha[largura][3];
+
+    // Processamento dos pixels
+    for (int i = 0; i < altura; i++)
+    {
+        for (int j = 0; j < largura; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                fscanf(entrada, "%d", &linha[j][k]);
+            }
+        }
+
+        if (i < altura / 2)
+        {
+            // Gravar metade superior
+            for (int j = 0; j < largura; j++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    fprintf(saidaSuperior, "%d ", linha[j][k]);
+                }
+            }
+            fprintf(saidaSuperior, "\n");
+        }
+        else
+        {
+            // Gravar metade inferior
+            for (int j = 0; j < largura; j++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    fprintf(saidaInferior, "%d ", linha[j][k]);
+                }
+            }
+            fprintf(saidaInferior, "\n");
+        }
+    }
+
+    printf("Imagem dividida com sucesso.\n");
+
+    fclose(entrada);
+    fclose(saidaSuperior);
+    fclose(saidaInferior);
+
+    return 0;
+}
+
 int menu()
 {
     int option;
@@ -213,6 +388,7 @@ int menu()
     printf("3. Espelhar uma imagem verticalmente.\n");
     printf("4. Dividir uma imagem em dois arquivos, metade esquerda e metade direita).\n");
     printf("5. Dividir uma imagem em dois arquivos, metade superior e metade inferior).\n");
+    printf("0. Sair\n");
     printf("Opção: ");
     scanf("%d", &option);
 
@@ -222,37 +398,38 @@ int menu()
 int main()
 {
     int option;
-    option = menu();
-
-    switch (option)
+    do
     {
-    case 1:
-        printf("\nConvertendo imagem para escala de cinza...\n");
-        escalaCinza();
-        break;
-
-    case 2:
-        printf("\nInvertendo imagem horizontalmente...\n");
-        inverteHorizontal();
-        break;
-
-    case 3:
-        printf("\nInvertendo imagem verticalmente...\n");
-        inverteVertical();
-        break;
-
-    case 4:
-        printf("Dividindo a imagem em metade esquerda e metade direita...");
-        break;
-
-    case 5:
-        printf("Dividindo a imagem em metade superior e metade inferior...");
-        break;
-
-    default:
-        printf("\nOpção Inválida!\n");
-        break;
-    }
-
+        option = menu();
+        switch (option)
+        {
+        case 1:
+            printf("\nConvertendo imagem para escala de cinza...\n");
+            escalaCinza();
+            break;
+        case 2:
+            printf("\nInvertendo imagem horizontalmente...\n");
+            inverteHorizontal();
+            break;
+        case 3:
+            printf("\nInvertendo imagem verticalmente...\n");
+            inverteVertical();
+            break;
+        case 4:
+            printf("\nDividindo a imagem em metade esquerda e metade direita...\n");
+            divisaoVertical();
+            break;
+        case 5:
+            printf("\nDividindo a imagem em metade superior e metade inferior...\n");
+            divisaoHorizontal();
+            break;
+        case 0:
+            printf("\nSaindo...\n");
+            break;
+        default:
+            printf("\nOpção Inválida!\n");
+            break;
+        }
+    } while (option != 0);
     return 0;
 }
